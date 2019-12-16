0510c532346fa2a8c8abb0535a780d2f36365f08
MinexAutomation <minex.automation@gmail.com>
2019-12-16 14:46:19 -0800

    Implemented the Visual Studio solution file path provider service.

---
+++ source/R5T.Smyni.Default/Code/Services/Implementations/VisualStudioSolutionFilePathProvider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using R5T.Lombardy;
+using R5T.Smyni.Types;
+
+
+namespace R5T.Smyni.Default
+{
+    public class VisualStudioSolutionFilePathProvider : IVisualStudioSolutionFilePathProvider
+    {
+        private IFileNameOperator FileNameOperator { get; }
+        private IStringlyTypedPathOperator StringlyTypedPathOperator { get; }
+
+
+        public VisualStudioSolutionFilePathProvider(IFileNameOperator fileNameOperator, IStringlyTypedPathOperator stringlyTypedPathOperator)
+        {
+            this.FileNameOperator = fileNameOperator;
+            this.StringlyTypedPathOperator = stringlyTypedPathOperator;
+        }
+
+        public string GetVisualStudioSolutionFilePath(string solutionDirectoryPath, string solutionName)
+        {
+            var solutionFileName = this.FileNameOperator.GetFileName(solutionName, FileExtensions.SolutionFileExtension);
+
+            var solutionFilePath = this.StringlyTypedPathOperator.GetFilePath(solutionDirectoryPath, solutionFileName);
+            return solutionFilePath;
+        }
+    }
+}
